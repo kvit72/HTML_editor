bf5ec2458a1f15ad603efc870204ba9fbe820a14
kvit7 <kvit72v@yandex.ru>
2023-04-11 16:40:40 +0300

    Создан класс FrameListener. В класс View добавлены приватные поля JTabbedPane tabbedPane, JTextPane htmlTextPane, JEditorPane plainTextPane.

---
+++ src/listeners/FrameListener.java
@@ -0,0 +1,18 @@
+package listeners;
+
+import Editor.View;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class FrameListener extends WindowAdapter {
+    private View view;
+
+    public FrameListener(View view) {
+        this.view = view;
+    }
+
+    public void windowClosing(WindowEvent windowEvent) {
+        view.exit();
+    }
+}
