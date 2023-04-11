574bf959a9cad8d2a70c1d410289cdd343b1aaa5
kvit7 <kvit72v@yandex.ru>
2023-04-11 16:18:19 +0300

    В класс Controller добавлены методы exit() и init(), написан метод main(). В класс View добавлены методы exit() и init().

--- src/Editor/View.java
+++ src/Editor/View.java
@@ -18,4 +18,12 @@ public class View extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
     }
+
+    public void init() {
+
+    }
+
+    public void exit() {
+        controller.exit();
+    }
 }
