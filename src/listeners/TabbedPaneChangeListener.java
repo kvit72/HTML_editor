47c86b3f6e1abdab6a75d229db3ad7e9523b94cc
kvit7 <kvit72v@yandex.ru>
2023-04-11 17:00:00 +0300

    В класс View добавлен метод selectedTabChanged(). Созданы классы TabbedPaneChangeListener и ExceptionHandler.

---
+++ src/listeners/TabbedPaneChangeListener.java
@@ -0,0 +1,18 @@
+package listeners;
+
+import Editor.View;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class TabbedPaneChangeListener implements ChangeListener {
+    private View view;
+
+    public TabbedPaneChangeListener(View view) {
+        this.view = view;
+    }
+
+    public void stateChanged(ChangeEvent e) {
+        view.selectedTabChanged();
+    }
+}
