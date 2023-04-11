5e92b4ecc6979b9cedb3a4579d0ab32bb09525ec
kvit7 <kvit72v@yandex.ru>
2023-04-11 16:07:49 +0300

    Созданы классы Controller и View.

---
+++ src/Editor/Controller.java
@@ -0,0 +1,18 @@
+package Editor;
+
+import javax.swing.text.html.HTMLDocument;
+import java.io.File;
+
+public class Controller {
+    private View view;
+    private HTMLDocument document;
+    private File currentFile;
+
+    public Controller(View view) {
+        this.view = view;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Editor/View.java
@@ -0,0 +1,21 @@
+package Editor;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class View extends JFrame implements ActionListener {
+    private Controller controller;
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public Controller getController() {
+        return controller;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
