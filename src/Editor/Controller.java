ac454b06282c6577dfff266511cf73392bedd93b
kvit7 <kvit72v@yandex.ru>
2023-04-13 16:12:33 +0300

    В классе Controller реализован метод createNewDocument.

--- src/Editor/Controller.java
+++ src/Editor/Controller.java
@@ -20,7 +20,7 @@ public class Controller {
     }
 
     public void init() {
-
+        createNewDocument();
     }
 
     public void exit() {
@@ -72,7 +72,10 @@ public class Controller {
     }
 
     public void createNewDocument() {
-
+        view.selectHtmlTab();
+        resetDocument();
+        view.setTitle("HTML редактор");
+        currentFile = null;
     }
 
     public void openDocument() {
