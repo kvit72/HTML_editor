bbb477e842ce286e81da84e3d5e5571a82444fc3
kvit7 <kvit72v@yandex.ru>
2023-04-13 15:49:39 +0300

    В классе View реализован метод selectedTabChanged.

--- src/Editor/View.java
+++ src/Editor/View.java
@@ -107,7 +107,15 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void selectedTabChanged() {
-
+        switch (tabbedPane.getSelectedIndex()) {
+            case 0:
+                controller.setPlainText(plainTextPane.getText());
+                break;
+            case 1:
+                plainTextPane.setText(controller.getPlainText());
+                break;
+        }
+        resetUndo();
     }
 
     public boolean canUndo() {
